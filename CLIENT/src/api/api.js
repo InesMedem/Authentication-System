43830c0ef4f68bcfc6427919c35a095738bc527c
InesMedem <inesmedem93@gmail.com>
2024-06-18 13:16:11 +0200

    app + api created

--- CLIENT/src/api/api.js
+++ CLIENT/src/api/api.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const instance = axios.creat({
+  baseURL: "http://localhost:3001",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
--- CLIENT/src/pages/Register.jsx
+++ CLIENT/src/pages/Register.jsx
@@ -1,5 +1,24 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <h2>User Registration</h2>
+      <form id="registrationForm">
+        <label htmlFor="username">Username:</label>
+
+        <input type="text" id="username" name="username" required />
+
+        <label htmlFor="email">Email:</label>
+
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="password">Password:</label>
+
+        <input type="password" id="password" name="password" required />
+
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
