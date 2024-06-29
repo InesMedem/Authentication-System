ae76f0931ffe2f13d1d796e55f3d50a3974ae94f
InesMedem <inesmedem93@gmail.com>
2024-06-29 10:36:37 +0200

    rearrangement index

--- SERVER/index.js
+++ SERVER/index.js
@@ -11,18 +11,18 @@ const app = express();
 
 //! Initialize environment variables
 dotenv.config();
-const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
 
 //! Connect to the database
 connect();
 
-//! Apply middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 configCloudinary();
 
+const PORT = process.env.PORT;
+// const MONGO_URI = process.env.MONGO_URI;
+
+app.use(cors());
+
+//! Apply middleware
 // app.use(cors());
 
 app.use(
@@ -32,6 +32,9 @@ app.use(
     credentials: true, // Include credentials if required
   })
 );
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: false }));
 
 //! ROUTES
 app.use("/auth", UserRoutes);
